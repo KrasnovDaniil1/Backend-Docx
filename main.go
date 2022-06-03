e2d29a827093752f2b916955c1a514388214bd54
KrasnovDaniil1 <kd69931@gmail.com>
2022-06-03 22:32:08 +0200

    start write server

--- main.go
+++ main.go
@@ -5,20 +5,26 @@ import (
 	// "http"
 	"app/user"
 	"fmt"
+	"net/http"
 )
 
+const portNumber = ":8080"
+
+func HomePageHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("We're live !!!"))
+}
+
 func main() {
+	var allUsers = []user.User{} // все пользователи
 
-	allUsers := []user.User{}
-	
-	user1 := user.User{Login: "Daniil", Password: "qwer", File: []string{}}
-
-	// user1.File = append(user1.file, "sdfs")
-
-	allUsers = append(allUsers, user1)
-	allUsers = append(allUsers, user1)
-
+	user.NewUser(&allUsers, "Daniil", "password")
+	// user.NewUser(&allUsers, "Daniil", "password")
+	// user.NewUser(&allUsers, "Danil", "password")
 
 	fmt.Println(allUsers)
+
+	http.HandleFunc("/", HomePageHandler)
+	fmt.Printf("Starting application on port %v\n", portNumber)
+	http.ListenAndServe(portNumber, nil)
 
 }
--- user/user.go
+++ user/user.go
@@ -3,14 +3,38 @@ package user
 import "fmt"
 
 type User struct {
+	ID              int
 	Login, Password string
 	File            []string
 }
 
-func Col() {
-	fmt.Println("run")
+// type file struct {
+// 	name,text string
+// }
+
+// func (user *User) AddNewFile(file0 *file) {
+// 	AllUsers = append(AllUsers, "sdfs")
+// }
+
+/*проверяет на идентичность логина*/
+
+func checkUser(allUsers []User, login string) bool {
+	for _, v := range allUsers {
+		if v.Login == login {
+			fmt.Println("Такой логин уже есть")
+			return false
+		}
+	}
+	fmt.Println("Уникальный логин")
+	return true
 }
 
-// func (user *user) AddNewFile(name ,text string) {
-	// 	user := file = append(user1.file, "sdfs")
-	// }
+/*создаёт нового пользователя*/
+
+func NewUser(allUsers *[]User, login, password string) {
+	if checkUser(*allUsers, login) {
+		user := User{ID: len(*allUsers), Login: login, Password: password, File: []string{}}
+		*allUsers = append(*allUsers, user)
+		fmt.Println("Новый пользователь создан")
+	}
+}
