3790607ffff9d4857f868eb8e63e0f145ef225db
KrasnovDaniil1 <kd69931@gmail.com>
2022-06-04 22:28:05 +0200

    add methods

---
+++ README.md
@@ -0,0 +1,14 @@
+# backend-docx
+
+GET /:user -- авторизация и получение данных
+POST /:user -- регистрация и получение данных
+GET /:user/:filename -- получение конкретного файла
+PUT /:user/:filename -- сохранить изменение файла
+POST /:user/:filename -- создать файл
+DELETE /:user?filename -- удалить файл
+
+
+
+
+<!-- DELETE /users/file/:filename -- получение доступа к файлу -->
+
--- main.go
+++ main.go
@@ -5,26 +5,31 @@ import (
 	// "http"
 	"app/user"
 	"fmt"
+
 	"net/http"
 )
 
-const portNumber = ":8080"
-
-func HomePageHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("We're live !!!"))
-}
+const portNumber = ":8000"
 
 func main() {
 	var allUsers = []user.User{} // все пользователи
 
 	user.NewUser(&allUsers, "Daniil", "password")
-	// user.NewUser(&allUsers, "Daniil", "password")
-	// user.NewUser(&allUsers, "Danil", "password")
-
 	fmt.Println(allUsers)
 
-	http.HandleFunc("/", HomePageHandler)
-	fmt.Printf("Starting application on port %v\n", portNumber)
-	http.ListenAndServe(portNumber, nil)
+	http.Handle("/", http.HandlerFunc(ExampleHandler))
+    http.ListenAndServe(":8080", nil)
 
+}
+
+func ExampleHandler(w http.ResponseWriter, r *http.Request) {
+    // This handler will run for all types of HTTP request, but we can use r.Method to 
+    // determine which method is being used and validate the request based on this.
+    if r.Method == http.MethodGet {
+        io.WriteString(w, "This is a get request")
+    } else if r.Method == http.MethodPost {
+        io.WriteString(w, "This is a post request")
+    } else {
+        io.WriteString(w, "This is a " + r.Method + " request")
+    }
 }
